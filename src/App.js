54cb240eeffc1d085ceccfeac6d4f79805f3d86d
CTP <ctp@CTPs-MBP.fios-router.home>
2021-01-20 15:05:28 -0500

    Updated App.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,49 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import Grid from '@material-ui/core/Grid';
+import Button from '@material-ui/core/Button';
+import ButtonGroup from '@material-ui/core/ButtonGroup';
+
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <React.Fragment>
+        <Grid class = "space">
+        <ButtonGroup variant="contained" color="primary" aria-label="contained primary button group">
+          <Button>1</Button>
+          <Button>2</Button>
+          <Button>3</Button>
+          <Button>/</Button>
+        </ButtonGroup>
+        </Grid>
+
+        <Grid class = "space">
+        <ButtonGroup variant="contained" color="primary" aria-label="contained primary button group">
+          <Button>4</Button>
+          <Button>5</Button>
+          <Button>6</Button>
+          <Button>*</Button>
+        </ButtonGroup>
+        </Grid>
+        <Grid class = "space">
+        <ButtonGroup variant="contained" color="primary" aria-label="contained primary button group">
+          <Button>7</Button>
+          <Button>8</Button>
+          <Button>9</Button>
+          <Button>--</Button>
+        </ButtonGroup>
+        </Grid>
+        <Grid class = "space">
+        <ButtonGroup variant="contained" color="primary" aria-label="contained primary button group">
+          <Button>0</Button>
+        < Button>+</Button>
+        </ButtonGroup>
+        </Grid>
+      </React.Fragment>
+
+
     </div>
   );
 }
