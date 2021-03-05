03f4d8f6369540d6de8da220b184bde529cafc5b
CTP <ctp@CTPs-MacBook-Pro.local>
2021-03-05 17:51:43 -0500

    two result functions

--- README.md
+++ README.md
@@ -1,16 +1,30 @@
 # Creating a Calculator
 
-## Purpose 
-For the user to enter in two or more numbers to either add, subtract, multiply or divide to get a result number.
+## Description
+Created a Scientific Calculator that includes basic mathematics and trigonometric functions.
+Used a queue to loop through numbers and operations.
 
 ## High level features / user stories
 As a user, I want to either add, subtract, multiply or divide two or more numbers so that I can see the result number.
 
 ## Detailed sketches
-Each small box that contains a number, math operators, off and dot repersents a button.
-The long rectangle box a top represents the calculator screen that displays the numbers, math operations and result number.
-![Sketch of a Calculator](Calculator1.jpg)
+Each small box that contains a number, math operators, and trignometric function repersents a button.
+At the top represents the calculator screen that displays the numbers, math operations and result number.
+![A Picture of the Calculator](Calculator.jpg)
 
 ## Pseudocode to explain the more complex parts
-when user clicks on a number button, the number is stored into a variable called num1 and the number is displayed on the screen. Then clicks on one of the math operators. Then the user clicks on another number button, the number is stored into a variable called num2 and the number is displayed on the screen.
-Finally they click on the equal sign button and the result number is displayed on the screen.
+ADD three or more numbers to do math operations with
+I would want to put each number in a queue data structure
+Example: 45+23-12
+45 -> queue[]
++  -> queue[]
+23 -> queue[]
+......etc 
+queue => [45,"+",23,"-","12"]
+result function 
+use a for loop to loop through the queue
+check to see if the first element is a number and store it into a variable num1
+ if the second element is a math operation either add, subtract or divide 
+ and if the next element is a number either add, subtract, or divide
+ store the results in dig
+ 
--- src/App.js
+++ src/App.js
@@ -20,89 +20,122 @@ class App extends React.Component {
       calcList: []
     };
   }
-  // CheckNumber() function add all numbers press to form one digit number
+  // CheckNumber() function add all numbers pressed to form one digit number
   checkNumber(number) {
-    //
+  
+    // used else/if statements to check whether a trigonometric function button was pressed
+    // used a callback is a function passed as an argument to another function.
+    // I used the callback fuction to updated display after num1
     if (this.state.currentOperation === "sin") {
       this.setState(
         {
           num1: 10 * this.state.num1 + number,
-        },
+        }, // What I put on line 34-36 will run after line 31-33 is done.
         () => {
-          this.setState({ display: "sin(" + this.state.num1 + ")" });
+          this.setState({ display: "sin(" + this.state.num1 + ")",
+           });
+           this.state.calcList.push(Math.sin(this.state.num1)); 
         }
       );
-      // A callback is a function passed as an argument to another function.
-      // I used the callback fuction to updated display after num1
+
+      
     } else if (this.state.currentOperation === "cos") {
       this.setState(
         {
           num1: 10 * this.state.num1 + number,
         },
         () => {
-          this.setState({ display: "cos(" + this.state.num1 + ")" });
-        }
+          this.setState({ display: "cos(" + this.state.num1 + ")",
+           });
+            this.state.calcList.push(Math.cos(this.state.num1));
+        }  
       );
+      
+
     } else if (this.state.currentOperation === "tan") {
       this.setState(
         {
           num1: 10 * this.state.num1 + number,
         },
         () => {
-          this.setState({ display: "tan(" + this.state.num1 + ")" });
+          this.setState({ display: "tan(" + this.state.num1 + ")" ,
+          });
+          this.state.calcList.push(Math.tan(this.state.num1));
         }
       );
     }
-
-    if (this.state.negative === "(-)") {
-      this.setState(
-        {
-          num1: 10 * this.state.num1 + number
-        },
-        () => {
-          this.setState({ display: this.state.num1 });
-        }
-      );
-    }
-
+    // else the number will display and store the number into num1
     else {
-      this.setState({
-        display: this.state.display + number,
-        num1: 10 * this.state.num1 + number,
-      });
+    // used the else statement to check if the variable called negative equals "-" 
+    // 3 -> display : 3
+    // + -> display : display + "+" = "3,+"
+    // -7 -> display : display + "-7" = "3,+,-7"
+      if(this.state.negative === "-")
+      {
+        this.setState({
+          display: this.state.display + number,
+          num1: -Math.abs(10 * this.state.num1 + number)
+        });
+      }
+      else
+      {
+        this.setState({
+          display: this.state.display + number,
+          num1: 10 * this.state.num1 + number,
+        });
+      }
     }
+
+    console.log( "calcList is " + this.state.calcList);
   }
 
- 
 
-  //mathOperators() stores in the current num1 number and reset num1 to 0.
+  //mathOperators() displays and use any math operation button pressed.
   mathOperators(opera) {
-    if (this.state.negative === "-") {
-      this.setState({
-        display: this.state.display + opera,
-        storeNum: -Math.abs(this.state.num1),
-        currentOperation: opera,
-        num1: 0,
-        negative: ""
-      })
+    //Created a new empty array
+    let math = []
+
+    // used the else/if statement 
+    if (this.state.negative === "-") 
+    {
+      math.push(-Math.abs(this.state.num1));
     }
-    // calcList: [...calcList, ...math] is taking whatever is in the calcList array and the math array and
+    
+    if(this.state.currentOperation === "cos")
+    {
+      math.push(this.state.answer);
+
+    }
+    else if(this.state.currentOperation === "sin")
+    {
+      math.push(this.state.answer);
+      
+    }
+    else if(this.state.currentOperation === "tan")
+    {
+      math.push(this.state.answer);
+    
+    }
+    else 
+    {
+      math.push(this.state.num1); 
+    }
+
+     // calcList: [...calcList, ...math] is taking whatever is in the calcList array and the math array and
     // putting it into the calcList array. the three dots (...) are a spread operator
-    else {
-      let math = []
-      // Hers's the problem
-      math.push(this.state.num1);
-      math.push(opera);
-      // Hers's the problem
-      this.setState({
-        display: this.state.display + opera,
-        storeNum: this.state.num1,
-        calcList: [...this.state.calcList, ...math],
-        currentOperation: opera,
-        num1: 0,
-      });
-    }
+    math.push(opera)
+    this.setState({
+      display: this.state.display + opera,
+      storeNum: this.state.num1,
+      calcList: [...this.state.calcList, ...math],
+      // currentOperation: opera,
+      num1: 0,
+    });
+    console.log( "calcList is " + this.state.calcList);
   }
+// tan(-2)= -2.18
+
+
   sin() {
     this.setState({
       currentOperation: "sin",
@@ -115,7 +148,6 @@ class App extends React.Component {
       display: "cos",
     });
   }
-
   async tangent() {
     this.setState({
       currentOperation: "tan",
@@ -125,61 +157,147 @@ class App extends React.Component {
   negative() {
     this.setState({
       negative: "-",
-    }, () => {this.setState({display: this.state.display + this.state.negative})});
+      display: this.state.display + "-"
+    });
   }
-  // The result() function is created to display the result number
-  result() {
-    // using an else/if statements to check if currentOperation has a certain math operator
-    // then use that math operator to get the result number and display the number.
-  this.state.calcList.push(this.state.num1);
-  console.log( "num1 is " + this.state.num1);
-  let i;
-  let dig = null 
-  let math 
-  let digTwo = 0
-  console.log("array list " + this.state.calcList)
-  while(this.state.calcList.length != 0)
+  //new function
+  result2()
   {
-    if(dig === null)
+    if(this.state.currentOperation !== "")
     {
-      dig = this.state.calcList.shift()
+      this.setState({
+        calcList: this.state.answer
+      });
+        console.log("clacList is now: "+ this.state.calcList); 
     }
     else
     {
-      math = this.state.calcList.shift()
-      console.log("math = "+ math)
-      digTwo = this.state.calcList.shift() 
-      console.log("digTwo = "+ digTwo)
-      if(math == "+")
+  
+      this.state.calcList.push(this.state.num1);
+      
+      console.log("clacList: "+ this.state.calcList); 
+      let sign = 0;
+      let sum = 0; 
+      while(this.state.calcList.length !== 1)
       {
-        dig = dig + digTwo
-        console.log(dig)
-      }
-      else if (math == "-")
-      {
-        dig = dig - digTwo
-        console.log(dig)
-      }
-      else if (math == "*")
-      {
-        dig = dig * digTwo
-      }
-      else if (math == "/")
-      {
-        dig = dig / digTwo
+
+        if(this.state.calcList.indexOf("*") !== -1)
+        {
+          sign = this.state.calcList.indexOf("*");
+          console.log(sign);
+          sum = this.state.calcList[sign-1] * this.state.calcList[sign+1];
+          console.log(sum);
+          // remove calcList[sign-1], calcList[sign], calcList[sign+1] replace with sum.
+          this.state.calcList.splice(sign-1,3,sum);
+          console.log("calcList looks like this now :" + this.state.calcList);
+        }
+        else if (this.state.calcList.indexOf("/") !== -1)
+        {
+          sign = this.state.calcList.indexOf("/");
+          sum = this.state.calcList[sign-1] / this.state.calcList[sign+1];
+          this.state.calcList.splice(sign-1,3,sum);
+        }
+        else if(this.state.calcList.indexOf("+") !== -1)
+        {
+          sign = this.state.calcList.indexOf("+");
+          console.log(sign);
+          sum = this.state.calcList[sign-1] + this.state.calcList[sign+1];
+          console.log(sum);
+          this.state.calcList.splice(sign-1,3,sum);
+          console.log(this.state.calcList);
+        }
+        else if(this.state.calcList.indexOf("-") !== -1)
+        {
+          sign = this.state.calcList.indexOf("-");
+          sum = this.state.calcList[sign-1] - this.state.calcList[sign+1];
+          this.state.calcList.splice(sign-1,3,sum);
+          console.log(this.state.calcList)
+        }
+
       }
     }
+        console.log("result number: " + this.state.calcList);
+
+      this.setState({
+        answer: this.state.calcList,
+        display: this.state.display + " = " + this.state.calcList
+      })
+      console.log( "calcList is " + this.state.calcList);
+    
+      
   }
-  console.log(dig);
+  // The result() function is created to calculate and display the result number
+  result() {
+    // using an else/if statements to check if currentOperation has a certain math operator
+    // then use that math operator to get the result number and display the number.
 
-  this.setState({
-    answer: dig,
-    display: this.state.display + " = " + dig
-  })
+    // this.setState({
+      // calcList: this.state.num1,
+    // });
+    this.state.calcList.push(this.state.num1);
+    console.log( "num1 is " + this.state.num1);
+  
+    let dig = null 
+    let math 
+    let digTwo = 0
+    console.log("array list " + this.state.calcList)
+    // example: calcList[2,+,9,*,3]
+    // calcList[2+27]
+    // claCList[29]
+    while(this.state.calcList.length !== 0)
+    {
+      if(dig === null)
+      {
+        dig = this.state.calcList.shift()
+      }
+      else if (this.state.currentOperation === 'sin')
+      {
+        dig = this.state.calcList.shift()
+      }
+      else if (this.state.currentOperation === 'cos')
+      {
+        dig = this.state.calcList.shift()
+      }
+      else if (this.state.currentOperation === 'tan')
+      {
+        dig = this.state.calcList.shift()
+      }
+      else
+      {
+        math = this.state.calcList.shift()
+        console.log("math = "+ math)
+        digTwo = this.state.calcList.shift() 
+        console.log("digTwo = "+ digTwo)
+        if(math === "+")
+        {
+          dig = dig + digTwo
+          console.log(dig)
+        }
+        else if (math === "-")
+        {
+          dig = dig - digTwo
+          console.log(dig)
+        }
+        else if (math === "*")
+        {
+          dig = dig * digTwo
+        }
+        else if (math === "/")
+        {
+          dig = dig / digTwo
+        }
+      }
+    }
+    console.log(dig);
 
+    this.setState({
+      answer: dig,
+      display: this.state.display + " = " + dig
+    })
+    console.log( "calcList is " + this.state.calcList);
   }
 
-  // The clear() function is created to clear the displayed screen
+  // The clear() function is created to clear the displayed screen and reset all the variables
   clear() {
     this.setState({
       num1: 0,
@@ -228,7 +346,7 @@ class App extends React.Component {
             <Button onClick={() => { this.clear(); }}><h1>C</h1></Button>
             <Button onClick={() => { this.checkNumber(0); }}><h1>0</h1></Button>
             < Button onClick={() => { this.mathOperators("+"); }}><h1>+</h1></Button>
-            < Button onClick={() => { this.result(); }}><h1>=</h1></Button>
+            < Button onClick={() => { this.result2(); }}><h1>=</h1></Button>
           </ButtonGroup>
         </Grid>
         <Grid class="space">
@@ -250,29 +368,37 @@ export default App;
 
 
 // ADD three or more numbers to do math operations with
-// I would want to put each number in a stack data structures
+// I would want to put each number in a queue data structures
 // Example: 45+23-12
-// 45 -> stack[]
-// +  -> stack[]
-// 23 -> stack[]
+// 45 -> queue[]
+// +  -> queue[]
+// 23 -> queue[]
 // ......etc 
-//stack[45,"+",23,"-","12"]
-//result function 
-// use a for loop to loop through the stack
+// queue[45,"+",23,"-","12"]
+// result function 
+// use a for loop to loop through the queue
 // check to see if the first element is a number and store it into a variable num1
 //  if the second element is a math operation either add, subtract or divide 
 //  and if the next element is a number either add, subtract, or divide
-//  store the results in storeNum
-//  
+//  store the results in dig
+ 
 
 
 
 /*
- stack[45,"+",23,"-","12"]
+ stack[45,"+",23,"-","12","*","3"]
  if stack[0] is a number
   num1 = stack[1]
  loop i = 1
  if stack[i] is a math operator
   if stack[i+1] is a number
     storeNum = storeNum +, -, /, or * num1
+
+    12 * 3 = 36
+
+  PEMDAS
+  M or D
+  
  */
+
+ /* */
